5189bad3f02108d26b2b7e6178c96a28efddb797
D.hamza <hamzademnati21@gmail.com>
2023-07-15 10:31:33 +0200

    add header to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem"
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        "drose":"#E60C7C",
+        "dblue":"#006BB6",
+        "dorange":"#F7952A"
       },
     },
   },
